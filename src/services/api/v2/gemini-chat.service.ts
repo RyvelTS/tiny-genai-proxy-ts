e3833a7eaa6e8bf4fb8c66de25a4213d975aba0e
Ryvel Stamber <ryvel.timothy@gmail.com>
2025-06-09 00:19:38 +0800

    update(v2): Refine function calls for improved client-side interactions

--- src/services/api/v2/gemini-chat.service.ts
+++ src/services/api/v2/gemini-chat.service.ts
@@ -5,23 +5,27 @@ import logger from "../../../utils/logger.js";
 class GeminiChatService {
     public async processChat(payload: GeminiChatRequestPayload) {
         const { systemPrompt, newUserMessage } = payload;
-        // const SYSTEM_MESSAGE_TAG = "[[SYS_EVAL_RESULT]]";
-        // const evaluationResult = await GeminiService.evaluatePromptSafety(systemPrompt, newUserMessage);
+        const SYSTEM_MESSAGE_TAG = "[[SYS_EVAL_RESULT]]";
+        const evaluationResult = await GeminiService.evaluatePromptSafety(systemPrompt, newUserMessage);
         let userMessageForModel = newUserMessage;
-        // if (evaluationResult.isMalicious) {
-        //     let evalResult = `${SYSTEM_MESSAGE_TAG} The previous user input was flagged as malicious. Reason: '${evaluationResult.reason}'. The original message has been withheld and will not be processed.`;
-        //     logger.warn(evalResult);
-        //     if (payload.conversationHistory) {
-        //         payload.conversationHistory.push({ role: 'model', parts: [evalResult] });
-        //         userMessageForModel = "Why can't you help me? And how can you assist me today?";
-        //     } else {
-        //         userMessageForModel = evalResult
-        //     }
-        // }
+        if (evaluationResult.isMalicious) {
+            let evalResult = `${SYSTEM_MESSAGE_TAG} The previous user input was flagged as malicious. Reason: '${evaluationResult.reason}'. The original message has been withheld and will not be processed.`;
+            logger.warn(evalResult);
+            if (payload.conversationHistory) {
+                payload.conversationHistory.push({ role: 'model', parts: [evalResult] });
+                userMessageForModel = "Why can't you help me? And how can you assist me today?";
+            } else {
+                userMessageForModel = evalResult
+            }
+        }
 
-        const aiResponseText = await GeminiService.generateResponse({ ...payload, newUserMessage: userMessageForModel });
-        logger.info(aiResponseText)
-        return aiResponseText;
+        const aiResponse = await GeminiService.generateResponse({ ...payload, newUserMessage: userMessageForModel });
+        logger.info(aiResponse)
+        return {
+            isMalicious: evaluationResult.isMalicious,
+            response: aiResponse,
+            reason: evaluationResult.reason
+        };
     }
 }
 
